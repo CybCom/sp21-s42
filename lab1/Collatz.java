ee793a34f706bdce63bbcfb70c91875195d1516c
CybCom <91059829+CybCom@users.noreply.github.com>
2022-04-10 18:41:04 +0800

    done with Collatz

--- lab1/Collatz.java
+++ lab1/Collatz.java
@@ -3,14 +3,21 @@
  */
 public class Collatz {
 
-    /** Buggy implementation of nextNumber! */
+    /** no Buggy implementation of nextNumber! */
     public static int nextNumber(int n) {
-        if (n  == 128) {
+        /**if (n  == 128) {
             return 1;
         } else if (n == 5) {
             return 3 * n + 1;
         } else {
             return n * 2;
+        } 草 */
+        if (n % 2 == 0) {
+            return n / 2;
+        } else if (n == 1) {
+            return 1;
+        } else {
+            return 3 * n + 1;
         }
     }
 
